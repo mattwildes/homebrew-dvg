b9dd793dc7b57f7ff082fbef9c1e23a5bcd22343
Matt Wildes <Matt@DVG-Matt.local>
2019-11-15 11:27:56 -0500

    Adding exmancmd

---
+++ Casks/exmancmd.rb
@@ -0,0 +1,9 @@
+cask 'exmancmd' do
+  version :latest
+  sha256 :no_check
+  
+  url 'http://www.adobe.com/go/ExManCmdMac'
+  name 'ExManCMD'
+  homepage 'https://partners.adobe.com/exchangeprogram/creativecloud/support/exman-com-line-tool.html'
+            
+end
